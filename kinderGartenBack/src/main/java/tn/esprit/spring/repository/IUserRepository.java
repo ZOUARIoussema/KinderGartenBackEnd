68f67d609f73ff9c0794ad68305e0ff4535fbdbe
Ahmeeedd <60976715+Ahmeeedd@users.noreply.github.com>
2021-02-27 02:54:41 +0100

    second commit

--- kinderGartenBack/src/main/java/tn/esprit/spring/repository/IUserRepository.java
+++ kinderGartenBack/src/main/java/tn/esprit/spring/repository/IUserRepository.java
@@ -1,9 +1,7 @@
 package tn.esprit.spring.repository;
 
-<<<<<<< HEAD
-=======
+
 import java.util.List;
->>>>>>> 99bf506d3e5cf36fdfc6f122a87288ec94174114
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
